3a3768cafb9b4add15daa6c6107b055e17e8d8f1
Armon ROHANI <arohani@e2r4p2.42.fr>
2018-05-24 17:08:24 +0200

    adds lexer functionality for testing

---
+++ includes/lexer.h
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antoipom <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/07 14:13:58 by antoipom          #+#    #+#             */
+/*   Updated: 2018/05/08 15:42:22 by antoipom         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+# define LEXER_H
+
+# include <stddef.h>
+
+enum
+{
+	ANY = 0,
+	SPACE,
+	TAB,
+	ESCAPE,
+	DQUOTE,
+	QUOTE,
+	NEWLINE,
+	IO_NUMBER,
+	GREAT,
+	LESS,
+	PIPE,
+	SEMI,
+	COMMENT,
+	AND,
+	END
+};
+
+enum
+{
+	TK_WORD,
+	TK_NEWLINE,
+	TK_IO_NUMBER,
+	TK_GREAT,
+	TK_DGREAT,
+	TK_GREATAND,
+	TK_LESS,
+	TK_DLESS,
+	TK_LESSAND,
+	TK_PIPE,
+	TK_SEMI,
+	TK_COMMENT,
+	TK_SPACE,
+	TK_AND,
+	TK_AND_IF,
+	TK_OR_IF,
+	TK_END
+};
+
+int						*get_tokens(char *line);
+
+#endif
---
+++ srcs/lexer/main.c
@@ -0,0 +1,83 @@
+#include "lexer.h"
+#include <stdio.h>
+
+int				main(int argc, char **argv)
+{
+	int *tab;
+	int i = 0;
+
+	if (argc == 2)
+	{
+		printf("%s\n", argv[1]);
+		tab = get_tokens(argv[1]);
+		while (tab[i] != -1)
+		{
+			switch(tab[i])
+			{
+				case TK_WORD:
+					printf("WORD ");
+					break;
+				case TK_NEWLINE:
+					printf("NEWLINE ");
+					break;
+				case TK_IO_NUMBER:
+					printf("IO_NUMBER ");
+					break;
+				case TK_GREAT:
+					printf("GREAT ");
+					break;
+				case TK_DGREAT:
+					printf("DGREAT ");
+					break;
+				case TK_GREATAND:
+					printf("GREATAND ");
+					break;
+				case TK_LESS:
+					printf("LESS ");
+					break;
+				case TK_DLESS:
+					printf("DLESS ");
+					break;
+				case TK_LESSAND:
+					printf("LESSAND ");
+					break;
+				case TK_PIPE:
+					printf("PIPE ");
+					break;
+				case TK_SEMI:
+					printf("SEMI ");
+					break;
+				case TK_COMMENT:
+					printf("COMMENT ");
+					break;
+				//case TK_SPACE:
+				//	printf("SPACE ");
+				//	break;
+				case TK_AND:
+					printf("AND ");
+					break;
+				case TK_AND_IF:
+					printf("AND_IF ");
+					break;
+				case TK_OR_IF:
+					printf("OR_IF ");
+					break;
+				case TK_END:
+					printf("END ");
+					break;
+				case TK_PROGRAM:
+					printf("PROGRAM ");
+					break;
+				case TK_QUOTED_WORD:
+					printf("QUOTED_WORD ");
+					break;
+				case TK_DQUOTED_WORD:
+					printf("DQUOTED_WORD ");
+					break;
+			}
+			i += 3;
+		}
+		printf("\n");
+	}
+	return (0);
+}
