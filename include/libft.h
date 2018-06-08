1ac4881b03f4fde7ca6a34ccd61e8edf57829cfb
Armon ROHANI <arohani@e2r11p3.42.fr>
2018-06-08 14:41:21 +0200

    updates libft.h files with strndup prototype, updates parser.c and ast.c, removes return value warning in ast_evaluate, reactivates traversing spaces to avoid spaces in tmp->arg...

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: azybert <azybert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/14 11:26:13 by azybert           #+#    #+#             */
-/*   Updated: 2017/12/04 05:06:17 by azybert          ###   ########.fr       */
+/*   Updated: 2018/06/08 14:11:19 by arohani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t			ft_strlen(const char *s);
 char			*ft_strdup(const char *s1);
+char			*ft_strndup(const char *s1, size_t n);
 char			*ft_strcpy(char *dst, const char *src);
 char			*ft_strncpy(char *dst, const char *src, size_t len);
 char			*ft_strcat(char *s1, const char *s2);
