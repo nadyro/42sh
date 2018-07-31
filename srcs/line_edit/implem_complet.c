e9175e5f5f308f6730e09f42ff6f20ad25617ece
calimoose42 <arohani@student.42.fr>
2018-07-31 13:11:28 +0200

    pulling updated line edit from qwerty to parser on july 31

---
+++ srcs/line_edit/implem_complet.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   implem_complet.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azybert <azybert@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/27 03:07:12 by azybert           #+#    #+#             */
+/*   Updated: 2018/07/30 06:12:41 by azybert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sh_line_edit.h"
+
+void	final_display(t_prompt *prompt, char *to_display)
+{
+	size_t	rpos;
+	char	*rline;
+
+	rpos = prompt->pos;
+	move_cursor(prompt, 0, true);
+	rline = prompt->line;
+	prompt->line = to_display;
+	prompt->total = ft_strlen(prompt->line);
+	write_data(prompt, prompt->line, prompt->total);
+	prompt->line = rline;
+	prompt->total = ft_strlen(prompt->line);
+	move_cursor(prompt, rpos, true);
+}
+
+void	completion_display_aux(t_prompt *prompt,
+		t_node *complete, int nb, int max)
+{
+	char	*to_display;
+	char	*spn;
+	int		count;
+
+	if (!(to_display =
+				ft_memalloc(sizeof(char) * nb * max + 1 + prompt->total + 1)))
+		exit(1);
+	ft_strcat(to_display, prompt->line);
+	ft_strcat(to_display, "\n");
+	count = 0;
+	while (complete)
+	{
+		ft_strcat(to_display, complete->cmd);
+		spn = ft_strchr(to_display, '\0');
+		if (++count % (prompt->size->x / max) == 0)
+			ft_strcat(to_display, "\n");
+		else
+			ft_memset(spn, ' ', max - ft_strlen(complete->cmd));
+		complete = complete->next;
+		(complete != NULL ? free(complete->prev->cmd) : 0);
+		(complete != NULL ? free(complete->prev) : 0);
+	}
+	final_display(prompt, to_display);
+	free(to_display);
+}
+
+void	completion_display(t_prompt *prompt, t_node *complete)
+{
+	t_node	*loop;
+	size_t	max;
+	int		nb;
+
+	loop = complete;
+	nb = 1;
+	max = ft_strlen(loop->cmd);
+	while (loop->next != NULL)
+	{
+		max = (ft_strlen(loop->next->cmd) > max ?
+				ft_strlen(loop->next->cmd) : max);
+		nb++;
+		loop = loop->next;
+	}
+	max++;
+	completion_display_aux(prompt, complete, nb, max);
+	free(loop->cmd);
+	free(loop);
+}
+
+void	auto_complete(t_prompt *prompt)
+{
+	t_node	*complete;
+	char	*arg;
+	char	*halp;
+	char	*to_free;
+
+	arg = ft_strndup(prompt->line, prompt->pos);
+	halp = (ft_strrchr(arg, ' ') ? ft_strrchr(arg, ' ') : 0);
+	to_free = arg;
+	if (halp == NULL)
+		arg = ft_strndup(prompt->line, prompt->pos);
+	else
+		arg = ft_strdup(halp);
+	free(to_free);
+	complete = fetch_names(arg);
+	while (complete && complete->prev)
+		complete = complete->prev;
+	if (complete && complete->next == NULL)
+	{
+		secure_stock(prompt, complete->cmd + ft_strlen(arg));
+		free(complete->cmd);
+		free(complete);
+	}
+	else if (complete)
+		completion_display(prompt, complete);
+	free(arg);
+}
---
+++ srcs/line_edit/write_data.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   write_data.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azybert <azybert@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/28 17:19:56 by azybert           #+#    #+#             */
+/*   Updated: 2018/07/28 19:21:41 by azybert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sh_line_edit.h"
+
+static void	ft_writenl(char *to_display, size_t tmp)
+{
+	size_t	k;
+	size_t	sp;
+
+	k = 0;
+	sp = 0;
+	while (sp + k < tmp)
+	{
+		if (to_display[sp + k] == '\n')
+		{
+			write(1, &to_display[sp], k + 1);
+			write(1, "\r", 1);
+			sp = sp + k + 1;
+			k = 0;
+		}
+		else
+			k++;
+	}
+	write(1, &to_display[sp], k + 1);
+	write(1, "\r", 1);
+}
+
+static void	write_data_aux(t_prompt *prompt, char *to_display, size_t size)
+{
+	size_t	tmp;
+	size_t	displayed;
+
+	displayed = prompt->total - ft_strlen(to_display);
+	while (size != 0)
+	{
+		move_cursor(prompt, displayed, false);
+		tmp = prompt->size->x - (ft_add_nl(prompt, displayed) +
+				prompt->origin->x) % prompt->size->x;
+		tmp = ((tmp > size) ? size : tmp);
+		ft_writenl(to_display, tmp);
+		displayed = displayed + tmp;
+		to_display += tmp;
+		size -= tmp;
+	}
+}
+
+void		write_data(t_prompt *prompt, char *to_display, size_t size)
+{
+	if (prompt->origin->y == 0xffffffffffffffff)
+		return ;
+	move_cursor(prompt, prompt->pos, true);
+	tputs(tgetstr("cd", NULL), 1, ft_putshit);
+	while (prompt->size->y <= prompt->origin->y + (prompt->origin->x +
+				ft_add_nl(prompt, prompt->pos + size)) / prompt->size->x)
+	{
+		move_cursor(prompt, (prompt->size->y - prompt->origin->y) *
+				prompt->size->x, false);
+		tputs(tgetstr("sf", NULL), 1, ft_putshit);
+		prompt->origin->y--;
+	}
+	if (prompt->origin->y == 0xffffffffffffffff)
+		return ;
+	write_data_aux(prompt, to_display, size);
+}
