d7da16b8ba4b495888c31362fdf10d375f6bc769
azybert <alex_zybert@hotmail.com>
2018-08-07 14:42:38 +0200

    small percent

--- srcs/line_edit/cursor_motion.c
+++ srcs/line_edit/cursor_motion.c
@@ -6,7 +6,7 @@
 /*   By: azybert <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 17:23:30 by azybert           #+#    #+#             */
-/*   Updated: 2018/08/07 13:29:12 by azybert          ###   ########.fr       */
+/*   Updated: 2018/08/07 14:40:55 by azybert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,14 @@ void	get_cursor_pos(t_coord *actualize, t_prompt *prompt)
 	{
 		actualize->x = 0;
 		actualize->y = 0;
+	}
+	if (actualize->x != 0)
+	{
+		actualize->x = 0;
+		write(1, "%", 1);
+		actualize->y++;
+		tputs(tgetstr("sf", NULL), 1, ft_putshit);
+		tputs(tgoto(tgetstr("cm", NULL), 0, actualize->y), 1, ft_putshit);
 	}
 }
 
