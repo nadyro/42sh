c4d38809c8f57bcbcacbfbe5345fbb1982ae315c
Armon ROHANI <arohani@e2r9p6.42.fr>
2018-08-08 18:32:14 +0200

    updates pipes, changed a bunch of things to handle cat | ls, EVERYONE TEST PIPES AGAIN

--- srcs/parser/pipes.c
+++ srcs/parser/pipes.c
@@ -6,7 +6,7 @@
 /*   By: arohani <arohani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/31 17:54:38 by arohani           #+#    #+#             */
-/*   Updated: 2018/08/01 14:50:43 by arohani          ###   ########.fr       */
+/*   Updated: 2018/08/08 18:27:07 by arohani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,39 +18,42 @@
 int        evaluate_pipe_node(t_shell *shell, t_ast *cmd)
 {
     pid_t       pid;
+	pid_t		wpid;
     int         fd[2];
-    t_ast       *ast = cmd;
 	int			status = 0;
+	int 		pfd[2];
 
-    pipe(fd);   //creates pipe read end (fd[0]) and write end (fd[1]), accessible by all children and parent
+    pfd[0] = dup(0);
+	pfd[1] = dup(1);
+	pipe(fd);   //creates pipe read end (fd[0]) and write end (fd[1]), accessible by all children and parent
     pid = fork();
 	if (pid == 0)	//first fork to execute left command in order to later read results by right command
 	{
 		dup2(fd[1], 1); // replaces stdout with pipe write end before executing child process 
 		close(fd[0]);   //closes pipe read end in child process
 		close(fd[1]);   //closes pipe write end in child process
-		ast_evaluate(ast->left, shell);		
-		(ast->left->cmd_ret == 0) ? exit (0) : exit(1);
+		ast_evaluate(cmd->left, shell);
+		//fprintf(stderr, "printed after evaluating left node of pipe node\n");
+		(cmd->left->cmd_ret == 0) ? exit(0) : exit(1);
 	}
+	else if (pid < 0)
+		exit (1);
 	else
 	{
-		pid = fork();	//second fork to execute right command and still return to ast
-		if (pid == 0)
-		{
-			dup2(fd[0], 0);		//replaces stdin with read end of pipe in order to read from left process before executing right process
-			close(fd[1]);
-			close(fd[0]);
-			ast_evaluate(ast->right, shell);
-			(ast->right->cmd_ret == 0) ? exit (0) : exit (1);
-		}
-		else
-		{
-			close(fd[0]);
-			close(fd[1]);
-			waitpid(pid, &status, 0);
-			if (status)
-				return (-1);
-			return (0);
-		}
+		dup2(fd[0], 0);
+		close(fd[0]);
+		close(fd[1]);
+		dup2(pfd[1], 1);
+		close(pfd[1]);
+		ast_evaluate(cmd->right, shell);
+		dup2(pfd[0], 0);
+		close(pfd[0]);
+		wpid = waitpid(pid, &status, WUNTRACED);
+		while (!WIFEXITED(status) && !WIFSIGNALED(status) && !WIFSTOPPED(status))
+			wpid = waitpid(pid, &status, WUNTRACED);
+		if (cmd->right->cmd_ret == -1)
+			return (-1);
+		return (0);
 	}
+	return (0);
 }
