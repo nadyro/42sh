76ab859c9a896bdab7696bdbe31de4f9660d81c2
Armon ROHANI <arohani@e2r12p10.42.fr>
2018-08-12 16:49:15 +0200

    forgot to add updated parser.h include file

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: arohani <arohani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/07 15:27:44 by arohani           #+#    #+#             */
-/*   Updated: 2018/08/11 19:46:23 by arohani          ###   ########.fr       */
+/*   Updated: 2018/08/12 16:34:27 by arohani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,12 @@ typedef struct	s_ast
 	t_redirs				*redirs;
 	struct s_ast			*right;
 }				t_ast;
+
 char			*token2str(int *token, char *str, char **env);
 int				parser_validation(int *tk_arr, char *ine);
+void			ast_loop_semi(t_ast *head, t_shell *shell, int i);
+void			ast_loop_and_or(t_ast *head, t_shell *shell, int i);
+void			ast_loop_pipe(t_ast *head, t_shell *shell, int i);
 void			create_arg_table(t_shell *shell, int beg, int end);
 int				get_tk_end_pos(t_shell *shell);
 t_ast			*get_ast(t_shell *shell);
@@ -58,7 +62,6 @@ int				ast_evaluate(t_ast *ast, t_shell *shell);
 t_ast			*fill_leftast(t_ast *parent);
 t_ast			*fill_rightast(t_ast *parent);
 t_ast			*init_ast(t_shell *shell);
-void			ast_loop(t_shell *shell, t_ast *ast);
 int				ast_execute(t_shell *shell, t_ast *cmd);
 int				*redirect_check(t_shell *shell);
 int				is_redirect(t_shell *shell, t_ast *ast, int beg, int end);
@@ -67,6 +70,16 @@ void			fill_redirs(t_shell *shell, t_ast *ast, int beg, int redir);
 void			implement_redirs(t_shell *shell, t_ast *cmd);
 void			restore_std_fds(t_shell *shell, t_ast *cmd, t_redirs *rd);
 int				evaluate_pipe_node(t_shell *shell, t_ast *cmd);
+void			implement_great(t_shell *shell, t_redirs *node, int fd);
+void			implement_dgreat(t_shell *shell, t_redirs *node, int fd);
+void			implement_greatand(t_shell *shell, t_redirs *node, int fd);
+void			implement_less(t_shell *shell, t_redirs *node, int fd);
+void			implement_lessand(t_shell *shell, t_redirs *node, int fd);
+void			implement_heredoc(t_ast *cmd, int id);
+int     		last_heredoc_id(t_shell *shell, t_redirs *node);
+void			redir_error(t_shell *shell, char *filename, int new_fd);
+void			shell_args_from_redirs(t_shell *shell, t_ast *cmd);
 void			free_ast(t_ast *head);
+void		    free_redirs(t_redirs *list);
 
 #endif
