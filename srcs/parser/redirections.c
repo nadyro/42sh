7b3d3cdec45a4c63a58bedb07d797cc428a2af07
Armon ROHANI <arohani@e2r12p10.42.fr>
2018-08-12 15:11:01 +0200

    norms redirections.c

--- srcs/parser/redirections.c
+++ srcs/parser/redirections.c
@@ -6,7 +6,7 @@
 /*   By: arohani <arohani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/19 12:59:04 by arohani           #+#    #+#             */
-/*   Updated: 2018/08/12 13:53:44 by arohani          ###   ########.fr       */
+/*   Updated: 2018/08/12 15:08:47 by arohani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,13 @@ void		redir_error(t_shell *shell, char *filename, int new_fd)
 	}
 }
 
-static int 	get_fd(t_shell *shell, t_redirs *node)
+static int	get_fd(t_shell *shell, t_redirs *node)
 {
-	t_redirs	*tmp = node;
-	int 		fd;
+	t_redirs	*tmp;
+	int			fd;
 	char		*str;
 
+	tmp = node;
 	if (tmp->next_re >= 3 && shell->tok[tmp->next_re - 3] == TK_IO_NUMBER)
 	{
 		str = shell->line + shell->tok[tmp->next_re - 3 + 1];
@@ -44,31 +45,35 @@ static int 	get_fd(t_shell *shell, t_redirs *node)
 	return (fd);
 }
 
-void 		implement_redirs(t_shell *shell, t_ast *cmd)
+static void	analyze_redir_node(t_shell *shell, t_redirs *tmp, int fd)
 {
-	t_redirs	*tmp = NULL;
-	int 		fd;
+	if (shell->tok[tmp->next_re] == TK_LESS)
+		implement_less(shell, tmp, fd);
+	else if (shell->tok[tmp->next_re] == TK_LESSAND)
+		implement_lessand(shell, tmp, fd);
+	else if (shell->tok[tmp->next_re] == TK_GREAT)
+		implement_great(shell, tmp, fd);
+	else if (shell->tok[tmp->next_re] == TK_GREATAND)
+		implement_greatand(shell, tmp, fd);
+	else if (shell->tok[tmp->next_re] == TK_DGREAT)
+		implement_dgreat(shell, tmp, fd);
+}
+
+void		implement_redirs(t_shell *shell, t_ast *cmd)
+{
+	t_redirs	*tmp;
+	int			fd;
 	int			last_id;
 
-	last_id = (ft_strstr(shell->line, "<<")) ? last_heredoc_id(shell, cmd->redirs) : -1;
+	last_id = (ft_strstr(shell->line, "<<")) ?
+		last_heredoc_id(shell, cmd->redirs) : -1;
 	cmd->hd_check = (last_id >= 0) ? 1 : 0;
 	shell_args_from_redirs(shell, cmd);
 	tmp = cmd->redirs;
 	while (tmp->next && shell->redir_error != 1)
-	{	
+	{
 		fd = get_fd(shell, tmp);
-		if (shell->tok[tmp->next_re] == TK_LESS)
-			implement_less(shell, tmp, fd);
-		else if (shell->tok[tmp->next_re] == TK_LESSAND)
-			implement_lessand(shell, tmp, fd);
-		else if (shell->tok[tmp->next_re] == TK_GREAT)
-			implement_great(shell, tmp, fd);
-		else if (shell->tok[tmp->next_re] == TK_GREATAND)
-			implement_greatand(shell, tmp, fd);
-		else if (shell->tok[tmp->next_re] == TK_DGREAT)
-			implement_dgreat(shell, tmp, fd);	
-	//	else if (shell->tok[tmp->next_re] == TK_DLESS)
-	//		implement_heredoc(tmp, counter++);
+		analyze_redir_node(shell, tmp, fd);
 		tmp = tmp->next;
 	}
 	if (last_id >= 0)
