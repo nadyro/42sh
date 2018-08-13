965ad942e6358b1d831f09a47abcd7ec701ade44
Armon ROHANI <arohani@e2r12p14.42.fr>
2018-08-13 14:18:56 +0200

    adds ft_strndup, gnl

--- libft/ft_strndup.c
+++ libft/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: arohani <arohani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/16 15:05:44 by arohani           #+#    #+#             */
-/*   Updated: 2018/02/19 15:51:25 by arohani          ###   ########.fr       */
+/*   Updated: 2018/08/09 07:01:16 by azybert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ char			*ft_strndup(const char *s1, size_t n)
 	i = 0;
 	if (!(str = (char *)malloc(sizeof(char) * n + 1)))
 		return (NULL);
-	while (s1[i] && i < n)
+	str[n] = '\0';
+	while (i < n)
 	{
 		str[i] = s1[i];
 		i++;
 	}
-	str[i] = '\0';
 	return (str);
 }
