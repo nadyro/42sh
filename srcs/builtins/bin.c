a5d16bdd38a1954500781b54bcbc199262d5f7f4
Armon Rohani <armonrohani@Armons-iMac.local>
2018-08-15 10:55:27 +0200

    fixes echo -s bug

--- srcs/builtins/bin.c
+++ srcs/builtins/bin.c
@@ -48,18 +48,20 @@ int			if_opt_n(t_shell *shell)
 int			ash_echo(t_shell *shell)
 {
 	int		i;
+	int 	n;
 
 	i = if_opt_n(shell);
+	n = (i == 2) ? 1 : 0;
 	if (shell->args && shell->args[i])
 	{
 		while (shell->args && shell->args[i])
 		{
-			if (i == 1 && shell->args[i][0] == '-')
-				i++;
+			//if (i == 1 && shell->args[i][0] == '-')
+			//	i++;
 			if (!(shell->args[i + 1]))
 			{
-				(!(ft_strcmp(shell->args[1], "-n"))) ?
-					ft_putstr(shell->args[i]) : ft_putendl(shell->args[i]);
+				(n == 1) ?	ft_putstr(shell->args[i]) :
+					ft_putendl(shell->args[i]);
 				return (0);
 			}
 			ft_putstr(shell->args[i++]);
