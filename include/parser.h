9f87994ce42065d0d24d56a342a93cf92a3367f6
Armon ROHANI <arohani@e2r12p17.42.fr>
2018-08-16 19:40:26 +0200

    norms all header files

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: arohani <arohani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/07 15:27:44 by arohani           #+#    #+#             */
-/*   Updated: 2018/08/16 17:03:33 by arohani          ###   ########.fr       */
+/*   Updated: 2018/08/16 19:35:08 by arohani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,11 +54,11 @@ void			implement_greatand(t_shell *shell, t_redirs *node, int fd);
 void			implement_less(t_shell *shell, t_redirs *node, int fd);
 void			implement_lessand(t_shell *shell, t_redirs *node, int fd);
 void			implement_heredoc(t_ast *cmd, int id);
-int     		last_heredoc_id(t_shell *shell, t_redirs *node);
+int				last_heredoc_id(t_shell *shell, t_redirs *node);
 void			redir_error(t_shell *shell, char *filename, int new_fd);
 void			shell_args_from_redirs(t_shell *shell, t_ast *cmd);
 void			free_ast(t_ast *head);
-void		    free_redirs(t_redirs *list);
+void			free_redirs(t_redirs *list);
 int				t2s_init(t_t2s **t2s);
 int				t2s_realloc(t_t2s *t2s, char buffer);
 int				t2s_init_b(t_t2s *t2s);
