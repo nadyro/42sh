ae6a76a6cf4da25066391dad33c13afd72ba1929
Armon ROHANI <arohani@e2r12p17.42.fr>
2018-08-16 19:33:52 +0200

    fixes dumb issue where command return values were always -10

--- srcs/parser/execute.c
+++ srcs/parser/execute.c
@@ -6,7 +6,7 @@
 /*   By: arohani <arohani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 15:01:35 by arohani           #+#    #+#             */
-/*   Updated: 2018/08/16 17:03:05 by arohani          ###   ########.fr       */
+/*   Updated: 2018/08/16 19:31:25 by arohani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ static void	launch_exec(t_shell *shell, char *full_path)
 			ft_putstr_fd(": Permission denied\n", 2);
 		}
 	}
-	sh_close(1, "");
+	exit(1);
 }
 
 static void	ast_launch(t_shell *shell, t_ast *cmd)
@@ -72,8 +72,7 @@ static void	ast_launch(t_shell *shell, t_ast *cmd)
 	else
 	{
 		wpid = waitpid(pid, &status, WUNTRACED);
-		if (status)
-			cmd->cmd_ret = -1;
+		cmd->cmd_ret = (status) ? -1 : 0;
 		while (!WIFEXITED(status) && !WIFSIGNALED(status)
 				&& !WIFSTOPPED(status))
 			wpid = waitpid(pid, &status, WUNTRACED);
