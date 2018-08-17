24ec9c134fbda6b2394ec624fda9cd887d32c6fb
Armon ROHANI <arohani@e2r11p17.42.fr>
2018-08-17 16:38:12 +0200

    replaces debugging mess with parser from QWERTY, need to execve full_path first

--- srcs/parser/execute.c
+++ srcs/parser/execute.c
@@ -6,7 +6,7 @@
 /*   By: arohani <arohani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 15:01:35 by arohani           #+#    #+#             */
-/*   Updated: 2018/08/17 16:34:41 by arohani          ###   ########.fr       */
+/*   Updated: 2018/08/16 19:31:25 by arohani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 #include "lexer.h"
 #include "libft.h"
 #define ARG shell->args[0]
-#include <stdio.h>
-extern int errno;
 
 void		restore_std_fds(t_shell *shell, t_ast *cmd, t_redirs *rd)
 {
@@ -45,19 +43,12 @@ void		restore_std_fds(t_shell *shell, t_ast *cmd, t_redirs *rd)
 
 static void	launch_exec(t_shell *shell, char *full_path)
 {
-	ft_putstr_fd("REMOVE THIS: entered launch exec\n", 2);
-	if (ARG && (!full_path || (full_path && execve(full_path, shell->args, shell->envv) == -1)))
+	if (execve(ARG, shell->args, shell->envv) == -1)
 	{
-		ft_putstr_fd("failed execution of full_path = ", 2);
-		ft_putendl_fd(full_path, 2);
-		if ((execve(ARG, shell->args, shell->envv) == -1))
+		if (full_path && execve(full_path, shell->args, shell->envv) == -1)
 		{
-			fprintf(stderr, "Here is the error message :\n%s\n", strerror(errno));
-			int i = 0;
-	//		ft_putstr_fd("failed 2nd attempt, searching in current directory with : ", 2);
-	//		ft_putendl_fd(ARG, 2);
-	//		ft_putstr_fd(ARG, 2);
-	//		ft_putstr_fd(": Permission denied\n", 2);
+			ft_putstr_fd(ARG, 2);
+			ft_putstr_fd(": Permission denied\n", 2);
 		}
 	}
 	exit(1);
@@ -119,43 +110,10 @@ static int	handle_arg_errors(t_shell *shell, t_ast *cmd)
 	return (0);
 }
 
-static void	pre_exec_check(t_shell *shell, t_ast *cmd)
+int			ast_execute(t_shell *shell, t_ast *cmd, int env_ex)
 {
 	struct stat	tmp;
 
-	fprintf(stderr, "DEBUG 1, ARG = %s\n", ARG);
-	if (stat(ARG, &tmp) == 0 && ((!(tmp.st_mode & (S_IXUSR))) || S_ISDIR(tmp.st_mode)))
-	{
-		fprintf(stderr, "DEBUG 2, ARG = %s\n", ARG);
-		if ((!(tmp.st_mode & (S_IXUSR))))
-		{
-			fprintf(stderr, "DEBUG 3\n");
-			if (!shell->full_path || (shell->full_path &&
-				stat(shell->full_path, &tmp) == 0 && (!(tmp.st_mode & (S_IXUSR)))))
-			{
-				fprintf(stderr, "DEBUG 4, full_path = %s\n", shell->full_path);
-				permission_denied(shell);
-				cmd->cmd_ret = -1;
-				return ;
-			}
-			fprintf(stderr, "DEBUG 5\n");
-		}
-		else
-		{
-			fprintf(stderr, "DEBUG 6\n");
-			executing_directory(shell);
-			cmd->cmd_ret = -1;
-			fprintf(stderr, "DEBUG 7\n");
-			return ;
-		}
-		fprintf(stderr, "DEBUG 8\n");
-	}
-	fprintf(stderr, "DEBUG 9, ARG = %s\n", ARG);
-	ast_launch(shell, cmd);	
-}
-
-int			ast_execute(t_shell *shell, t_ast *cmd, int env_ex)
-{
 	if (shell && shell->args && ARG && shell->redir_error != 1)
 	{
 		if (env_ex == 1 || ((cmd->cmd_ret = builtin_check(shell, cmd)) == -10))
@@ -164,8 +122,15 @@ int			ast_execute(t_shell *shell, t_ast *cmd, int env_ex)
 					has_paths(shell, 0) == 1) ? arg_full_path(shell) : NULL;
 			if (handle_arg_errors(shell, cmd) == 0)
 			{
-				fprintf(stderr, "about to send to pre_exec_check after no errors found\n");
-				pre_exec_check(shell, cmd);
+				if (stat(ARG, &tmp) == 0 && (S_ISDIR(tmp.st_mode) ||
+				!(tmp.st_mode & (S_IXUSR))))
+				{
+					(S_ISDIR(tmp.st_mode)) ? executing_directory(shell) :
+					permission_denied(shell);
+					cmd->cmd_ret = -1;
+				}
+				else
+					ast_launch(shell, cmd);
 			}
 		}
 	}
